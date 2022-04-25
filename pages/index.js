016f97443bff7d742d9ca8ffa9381498a0bcae2d
Justin Olmanson <olmansju@gmail.com>
2022-04-25 14:57:16 -0500

    made small edits

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to your <a href="https://nextjs.org">Next.js app!</a>
+          Welcome to my <a href="https://nextjs.org">Next.js app!</a>
         </h1>
 
         <p className={styles.description}>
