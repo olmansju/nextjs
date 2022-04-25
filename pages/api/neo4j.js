e7b95f499c9d9b7feb5e1c96bbe6bb0c2e345a2a
Justin Olmanson <olmansju@gmail.com>
2022-04-25 17:18:58 -0500

    made small edits

--- pages/api/neo4j.js
+++ pages/api/neo4j.js
@@ -1,35 +1,19 @@
+import neo4j from "neo4j-driver";
+
+const uri = 'neo4j+s://06f06c4a.databases.neo4j.io';
+const user = 'neo4j';
+const password = 'eKJRSyepu8l6wEFFjnfqWo-jUZeTKGnbJX48QehyFvQ';
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+
 export default async function handler(req, res) {
+    const session = driver.session();
+    const response = await session.run('MATCH (w) RETURN LABELS(w), COUNT(w) AS count');
 
-    const neo4j = require('neo4j-driver')
+    const records = response.records.map((record) => {
+        return record.toObject();
+    })
 
-    const uri = 'neo4j+s://06f06c4a.databases.neo4j.io';
-    const user = 'neo4j';
-    const password = 'eKJRSyepu8l6wEFFjnfqWo-jUZeTKGnbJX48QehyFvQ';
+    res.status(200).json({ body: records })
+}
 
-    const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
-    const session = driver.session()
-
-    const person1Name = 'Justin'
-
-    try {
-        // To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-        // The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-        const readQuery = `MATCH (u:User)
-                      WHERE u.name = 'Justin'
-                      RETURN u.name AS name`
-        const readResult = await session.readTransaction(tx =>
-            tx.run(readQuery, { personName: person1Name })
-        )
-        readResult.records.forEach(record => {
-            console.log(`Found person: ${record.get('name')}`)
-        })
-    } catch (error) {
-        console.error('Something went wrong: ', error)
-    } finally {
-        await session.close()
-    }
-    // Don't forget to close the driver connection when you're finished with it
-    await driver.close()
-
-    res.status(200).json({ name: 'Justin Doe' });
-};
